21b3b2c0f43b9ddc847a9b082f7f0292eb5d7462
Jack Pearkes <jackpearkes+git@gmail.com>
2012-03-19 17:40:55 -0700

    new-soo

--- images.py
+++ images.py
@@ -3,5 +3,6 @@ images = [
     'http://distilleryimage2.s3.amazonaws.com/0f1acefe696711e1989612313815112c_7.jpg',
     'http://sad-soo.s3.amazonaws.com/hot-startup.jpg',
     'http://sad-soo.s3.amazonaws.com/emails.jpg',
-    'http://sad-soo.s3.amazonaws.com/greenshirt.jpg'
+    'http://sad-soo.s3.amazonaws.com/greenshirt.jpg',
+    'http://sad-soo.s3.amazonaws.com/pelligrino.jpg'
     ]
